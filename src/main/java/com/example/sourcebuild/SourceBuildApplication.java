ad4e0141ac5cf718cfed21c324290f7b0e755b8d
Praveen Sinha <psinha@paloaltonetworks.com>
2022-09-15 23:58:18 -0700

    Initial commit

---
+++ src/main/java/com/example/sourcebuild/SourceBuildApplication.java
@@ -0,0 +1,13 @@
+package com.example.sourcebuild;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SourceBuildApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SourceBuildApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sourcebuild/SourceBuildApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sourcebuild;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SourceBuildApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
